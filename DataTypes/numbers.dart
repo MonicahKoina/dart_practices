ecb397adedd9a00a5ec54c76607851eb5e3044b7
Monicah Koina <monicahkoina1@gmail.com>
2024-12-24 19:02:09 +0300

    numbers

---
+++ DataTypes/numbers.dart
@@ -0,0 +1,9 @@
+//The number types represents numeric values
+void main() {
+  int i = 13;
+  num j = 17;
+  num sum = i + j;
+  print("The first number is $i");
+  print("The second number is $j");
+  print("The sum of $i and $j is $sum");
+}
